19e9c1250b15f1e1c5295809417a42496f51c217
曹操 <xuzhao.sgn@gmail.com>
2023-12-12 19:04:17 +0800

    rebrand

--- src/components/AvatarCard.tsx
+++ src/components/AvatarCard.tsx
@@ -22,17 +22,20 @@ export function AvatarCard({
     <div className='bg-#172636/10 border-[1px] border-#1A2835 rounded-[15px] group hover:bg-#5B6872/10 h-[280px] relative'>
       <div className='px-6 py-6 group-hover:px-4'>
         <div className='block group-hover:hidden'>
-          <img src={image} className="rounded-full w-32 h-32 mx-auto"/>
+          <img src={image} className="rounded-full border w-32 h-32 mx-auto"/>
           <h3 className='text-center mt-6 md:text-base text-#9DAFBE font-bold'>{title}</h3>
           <p className='text-center mt-4 md:text-base text-#5B6872'>{name}</p>
         </div>
         <div className='hidden group-hover:block'>
           <p className='text-center text-white text-sm leading-relaxed'>{description}</p>
           <div className='absolute inset-x-0 bottom-0 pb-6'>
-            <Link href={twitter_url} target="_blank">
-              <img src='/image/twitter_logo.png' className='w-12 mx-auto hidden md:block'/>
-              <p className='text-white/50 text-sm text-center mt-2 hidden md:block'>{twitter_username}</p>
-            </Link>
+            { 
+              twitter_url != "/" &&
+              <Link href={twitter_url} target="_blank">
+                <img src='/image/twitter_logo.png' className='w-12 mx-auto hidden md:block'/>
+                <p className='text-white/50 text-sm text-center mt-2 hidden md:block'>{twitter_username}</p>
+              </Link>
+            }
           </div>
         </div>
       </div>
